64498a51b31c2657d4df7766b650e6cd194c95e5
Änanta <ianantashrestha@gmail.com>
2023-03-06 11:54:06 +0545

    initial commit

---
+++ src/system/Document/index.tsx
@@ -0,0 +1,27 @@
+import Head from "next/head";
+import { ReactElement, ReactNode } from "react";
+interface DocumentProps {
+  title?: string;
+  children?: ReactNode;
+  meta?: ReactElement;
+  links?: ReactElement;
+}
+export default function document({
+  children,
+  meta,
+  title,
+  links,
+}: DocumentProps) {
+  return (
+    <>
+      <Head>
+        <title>Himalayan Trekking-{title}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {meta && meta}
+        <link rel="icon" href="/favicon.ico" />
+        {links && links}
+      </Head>
+      <main>{children}</main>
+    </>
+  );
+}
